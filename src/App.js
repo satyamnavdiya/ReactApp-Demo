c1658975593dd032ea9a8aafb3202a8204f98ca9
satyam navdiya <satyamnavdiya@gmail.com>
2024-06-08 14:51:02 +0530

    add new file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar'
-import TextForm from './components/TextForm'
+// import TextForm from './components/TextForm'
+import AboutUs from './components/AboutUs';
 
 function App() {
   return (
     <> 
       <Navbar title="SIT" aboutText="Contact Us" />
-      <TextForm heading="Enter Text to Analyse Below" />
+      {/* <TextForm heading="Enter Text to Analyse Below" /> */}
+      <AboutUs heading="About Us" />
     </>
   );
 }
