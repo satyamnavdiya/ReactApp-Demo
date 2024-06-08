3ef68d0b8999040d254c481fd3ad3e3dd34e43ce
satyam navdiya <satyamnavdiya@gmail.com>
2024-06-08 15:21:34 +0530

    AboutUs With Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar'
-import TextForm from './components/TextForm'
+import AboutUs from './components/AboutUs';
 
 function App() {
   return (
     <> 
       <Navbar title="SIT" aboutText="Contact Us" />
-      <TextForm heading="Enter Text to Analyse Below" />
+      <AboutUs />
     </>
   );
 }
