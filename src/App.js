bf92c22e4fe308513023d582694b07958c4117bb
satyam navdiya <satyamnavdiya@gmail.com>
2024-06-08 16:48:16 +0530

    half Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
-// import TextForm from './components/TextForm'
-import AboutUs from './components/AboutUs';
+import TextForm from './components/TextForm'
 
 function App() {
+
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = () =>{
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.background= 'black'
+    } else {
+      setMode('light');
+      document.body.style.background= 'white'
+    }
+  }
+
   return (
     <> 
-      <Navbar title="SIT" aboutText="Contact Us" />
+      <Navbar title="SIT" aboutText="Contact Us" mode={mode} toggleMode ={toggleMode} />
       <TextForm heading="Enter Text to Analyse Below" />
-      {/* <AboutUs heading="About Us" /> */}
     </>
   );  
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+            <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode} `}>
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,8 +24,9 @@ export default function Navbar(props) {
                             </li>
                         </ul>
                         <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-warning" type="submit">Search</button>
+                            <div className="form-check form-switch">
+                                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode} />{props.mode}
+                            </div>
                         </form>
                     </div>
                 </div>
@@ -34,13 +35,13 @@ export default function Navbar(props) {
     )
 }
 
-Navbar.propTypes={
-    title : PropTypes.string.isRequired,
-    aboutText : PropTypes.string
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    aboutText: PropTypes.string
 }
 
-Navbar.defaultProps={
-    title : "Set Your Title Here",
-    aboutText : "set your text here",
+Navbar.defaultProps = {
+    title: "Set Your Title Here",
+    aboutText: "set your text here",
 
 }
