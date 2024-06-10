225e1753b7d97e31edc5845fbb1790b5e1ba7863
satyam navdiya <satyamnavdiya@gmail.com>
2024-06-10 16:57:13 +0530

    Enable Dark & Light Mode

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ import TextForm from './components/TextForm'
 
 function App() {
 
-  const [mode, setMode] = useState('light')
+  const [mode, setMode] = useState('light');
+
+  const [btnText, newbtnText]= useState('Enable Dark Mode')
 
   const toggleMode = () =>{
     if (mode === 'light') {
       setMode('dark');
-      document.body.style.background= 'black'
+      document.body.style.background= 'black';
+      document.body.style.color= 'white';
     } else {
       setMode('light');
-      document.body.style.background= 'white'
+      document.body.style.background= 'white';
+      document.body.style.color= 'black';
     }
   }
 
   return (
     <> 
-      <Navbar title="SIT" aboutText="Contact Us" mode={mode} toggleMode ={toggleMode} />
-      <TextForm heading="Enter Text to Analyse Below" />
+      <Navbar title="SIT" aboutText="Contact Us" mode={mode} toggleMode ={toggleMode} btnText={btnText} />
+      <TextForm heading="Enter Text to Analyse Below" mode={mode} toggleMode={toggleMode} />
     </>
   );  
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -48,7 +48,7 @@ export default function TextForm(props) {
         <div>
             <div className="container my-5">
                 <h1>{props.heading}</h1>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
+                <textarea className={`form-control bg-${props.mode === 'light' ? 'light': 'dark'} text-${props.mode === 'light' ? 'dark': 'light'}`} id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
                 
                 <button className='btn btn-outline-success my-3' onClick={handleUpChange}>Convert to Upper Case</button>
                 <button className='btn btn-outline-success my-3 mx-3' onClick={handleLoChange}>Convert to Lower Case</button>
